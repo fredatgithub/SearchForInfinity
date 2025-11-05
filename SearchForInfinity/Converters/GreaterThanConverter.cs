ae30978c71413a0bf517f2b14f2604fd995aff15
Freddy JUHEL <PFJL06651@commun.ad.sncf.fr>
2025-11-05 18:12:15 +0100

    color background to light green when there is at least 1 line found with infinity value

---
+++ SearchForInfinity/Converters/GreaterThanConverter.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SearchForInfinity.Converters
+{
+    public class GreaterThanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null) 
+                return false;
+
+            double valueToCompare;
+            double compareValue;
+
+            if (!double.TryParse(value.ToString(), out valueToCompare) || 
+                !double.TryParse(parameter.ToString(), out compareValue))
+                return false;
+
+            return valueToCompare > compareValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
